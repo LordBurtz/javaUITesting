46d601959251989d7da9411eb78eb57847e10fe9
LordBurtz <fridolin.karger@gmail.com>
2021-07-12 20:15:10 +0200

    panels

--- src/main/java/SwingBroCode.java
+++ src/main/java/SwingBroCode.java
@@ -8,34 +8,41 @@ public class SwingBroCode {
     ImageIcon icon;
     JLabel label;
     Border border;
-    JPanel panel;
+    JPanel red_panel, blue_panel, green_panel;
 
     public SwingBroCode() {
         frame = new JFrame();
+        red_panel = new JPanel();
+        blue_panel = new JPanel();
+        green_panel = new JPanel();
         label = new JLabel();
-        border = BorderFactory.createLineBorder(new Color(0x559999), 1);
-
         icon = new ImageIcon("src/main/resources/logos/kei_logo.png");
 
-        label.setText("do you even code?");
+        label.setText("[sample text]");
         label.setIcon(icon);
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.BOTTOM);
-        label.setForeground(Color.lightGray);
-        label.setFont(new Font("Comic Sans", Font.BOLD, 20));
-        label.setBackground(Color.darkGray);
-        label.setOpaque(true);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        //label.setBounds(0, 0, 288, 388);
+
+        red_panel.setBackground(Color.red);
+        red_panel.setBounds(0, 0, 350, 350);
+        red_panel.add(label);
+        red_panel.setBorder(BorderFactory.createLineBorder(Color.cyan));
+
+        blue_panel.setBackground(Color.blue);
+        blue_panel.setBounds(250, 0, 300, 300);
+
+        green_panel.setBackground(new Color(0x339955));
+        green_panel.setBounds(0, 350, 750, 200);
 
         frame.setTitle("SwingBroCode testing GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 420);
+        frame.setSize(500, 500);
         frame.setVisible(true);
         frame.setIconImage(icon.getImage());
-        frame.add(label);
+
+        frame.setLayout(null);
+
+        frame.add(red_panel);
+        frame.add(blue_panel);
+        frame.add(green_panel);
     }
 
     public static void main(String[] args) {
