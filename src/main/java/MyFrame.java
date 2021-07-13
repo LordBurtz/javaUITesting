bdd2be020b131e23de4b32fce37fcb4c8c4780d1
LordBurtz <fridolin.karger@gmail.com>
2021-07-13 16:24:13 +0200

    button

--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -1,17 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
     ImageIcon icon;
+    JButton button;
+
     public MyFrame() {
         icon = new ImageIcon("src/main/resources/logos/kei_logo.png");
+        button = new JButton();
 
-        setTitle("SwingBroCode testing GUI");
+        button.setBounds(200, 100, 300, 50);
+        button.setFocusable(false);
+        button.setIcon(icon);
+        button.addActionListener(e -> {System.out.println("ping");});
+        button.setText("pwease cwick mew sewnpaiw oWo");
+
+        setTitle("fancy GUI");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //.setResizable(false);
-        setSize(420, 420);
+        setSize(600, 600);
         setVisible(true);
         setIconImage(icon.getImage());
-        getContentPane().setBackground(new Color(99, 64, 119));
+        setLayout(null);
+        add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == button) {
+            System.out.printf("ping");
+        }
     }
 }
--- src/main/java/SwingBroCode.java
+++ src/main/java/SwingBroCode.java
@@ -11,41 +11,11 @@ public class SwingBroCode {
     JPanel red_panel, blue_panel, green_panel;
 
     public SwingBroCode() {
-        frame = new JFrame();
-        red_panel = new JPanel();
-        blue_panel = new JPanel();
-        green_panel = new JPanel();
-        label = new JLabel();
-        icon = new ImageIcon("src/main/resources/logos/kei_logo.png");
 
-        label.setText("[sample text]");
-        label.setIcon(icon);
-
-        red_panel.setBackground(Color.red);
-        red_panel.setBounds(0, 0, 350, 350);
-        red_panel.add(label);
-        red_panel.setBorder(BorderFactory.createLineBorder(Color.cyan));
-
-        blue_panel.setBackground(Color.blue);
-        blue_panel.setBounds(250, 0, 300, 300);
-
-        green_panel.setBackground(new Color(0x339955));
-        green_panel.setBounds(0, 350, 750, 200);
-
-        frame.setTitle("SwingBroCode testing GUI");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setVisible(true);
-        frame.setIconImage(icon.getImage());
-
-        frame.setLayout(null);
-
-        frame.add(red_panel);
-        frame.add(blue_panel);
-        frame.add(green_panel);
     }
 
     public static void main(String[] args) {
-        SwingBroCode c = new SwingBroCode();
+        //SwingBroCode c = new SwingBroCode();
+        new MyFrame();
     }
 }
